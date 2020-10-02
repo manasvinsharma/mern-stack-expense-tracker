3c15d025127f6ba28469838a04ed02eebb1ebe8b
manasvinsharma <55654162+manasvinsharma@users.noreply.github.com>
2020-10-02 16:15:59 +0530

    Update server.js
    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-// const morgan = require('morgan');
 const connectDB = require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
